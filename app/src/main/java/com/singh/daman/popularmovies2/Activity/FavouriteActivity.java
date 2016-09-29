484cb042feb61f39b8afe10dd5694bff03cd3299
Damanpreet Singh <singhdaman4321@gmail.com>
2016-09-29 20:10:52 +0530

    favourite Activity

---
+++ app/src/main/java/com/singh/daman/popularmovies2/Activity/FavouriteActivity.java
@@ -0,0 +1,15 @@
+package com.singh.daman.popularmovies2.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.singh.daman.popularmovies2.R;
+
+public class FavouriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite);
+    }
+}
