13aa44686ad85f13c34135e8f5315dc33a7467f4
Damanpreet Singh <singhdaman4321@gmail.com>
2016-10-03 22:36:44 +0530

    ui changed

--- app/src/main/java/com/singh/daman/popularmovies2/Activity/MainActivity.java
+++ app/src/main/java/com/singh/daman/popularmovies2/Activity/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         if (findViewById(R.id.movies_detail_container) != null) {
             mTwoPane = true;
         } else {
